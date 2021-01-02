1e83621a595ca9a334f91242f63c8d5d3b8204f6
bestnub <63472070+bestnub@users.noreply.github.com>
2021-01-02 10:43:06 +0100

    Create test_gpu.py

---
+++ test_gpu.py
@@ -0,0 +1,18 @@
+# picks up the GPU it seems
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+import os
+
+
+#print(os.environ.get("CUDA_PATH"))
+
+#os.environ["CUDA_PATH"] = "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.0"
+
+
+#print(os.environ.get("CUDA_PATH"))
+
+
+print(device_lib.list_local_devices())
+
+
+#tf.test_is_gpu_available() 
